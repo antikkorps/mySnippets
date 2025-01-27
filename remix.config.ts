3f57a0b61ac357a5cfe19b01bf435dfac53fac91
Franck Vienot <franckvienot7@gmail.com>
2025-01-27 23:43:09 +0100

    add a vercel config

--- remix.config.ts
+++ remix.config.ts
@@ -1,11 +1,12 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: "vercel",
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: ["**/.*"],
   serverDependenciesToBundle: ["lucide-react"],
-  // Ajout du preset Vercel
   future: {
     unstable_postcss: true,
+    v2_errorBoundary: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
   },
 }
